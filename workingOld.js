3393c01f2e70f5f614f349b4bc33fe8b01c6607d
Srimali Udayangani <srimali.ranawaka@1billiontech.com>
2023-08-16 02:23:13 +0530

    button changes

--- workingOld.js
+++ workingOld.js
@@ -16,12 +16,12 @@ export function Pin(props) {
                     <View style={[{ paddingTop: 10, width: 35, height: 35, alignItems: "center" }]}>
                         <View style={[styles.pinTop]}></View>
                         <View style={[styles.pinBottom]}></View>
-                        <RadialGradient 
-                                style={{width:50,height:50, top: -20, opacity: 0.5}}
-                                colors={['#eb4034','transparent']}
-                                stops={[0.01]}
-                                >
-                            </RadialGradient>
+                        <RadialGradient
+                            style={{ width: 50, height: 50, top: -20, opacity: 0.5 }}
+                            colors={['#eb4034', 'transparent']}
+                            stops={[0.01]}
+                        >
+                        </RadialGradient>
                     </View>
                 </ImageBackground>
             ) : (
@@ -30,13 +30,13 @@ export function Pin(props) {
                     <View style={[styles.pinBottom]}></View>
                     {
                         props.shadow ? (
-                            <RadialGradient 
-                                style={{width:50,height:50, top: -20, opacity: 0.5}}
-                                colors={['#eb4034','transparent']}
+                            <RadialGradient
+                                style={{ width: 50, height: 50, top: -20, opacity: 0.5 }}
+                                colors={['#eb4034', 'transparent']}
                                 stops={[0.01]}
-                                >
+                            >
                             </RadialGradient>
-                        ): null
+                        ) : null
                     }
                 </View>
             )}
@@ -85,7 +85,7 @@ export default function WorkingOld() {
                 setActiveNewDraggable(false);
                 setActiveEditDraggable(false);
                 setActiveNewDraggableId(null);
-                
+
                 const preArray = cloneDeep(pins);
                 const id = activePin?.id || (preArray?.length || 0) + 1;
                 preArray.push({ x: x - wrapperXValue, y: y - wrapperYValue, id });
@@ -101,7 +101,7 @@ export default function WorkingOld() {
         setActiveEditDraggable(true);
         setActiveNewDraggable(false);
         setDropPin(true);
-        
+
         const copiedPins = cloneDeep(pins);
         const newSavedPins = copiedPins.filter((i) => i.id !== item.id);
         setPins([...newSavedPins]);
@@ -152,7 +152,7 @@ export default function WorkingOld() {
     const deleteItem = () => {
         const copiedPins = cloneDeep(pins);
         const newPins = copiedPins.filter((i) => i.id !== activePin.id);
-        
+
         setPins(newPins);
         setActiveNewDraggable(true);
         setActiveNewDraggableId(null);
@@ -166,7 +166,7 @@ export default function WorkingOld() {
     };
 
     return (
-            <View style={styles.container}>
+        <View style={styles.container}>
             <View style={styles.imageWrapper} ref={anchorRef}>
                 <Image source={require("./600x600.jpg")} style={styles.imageStyles} />
                 {/* 400 400 */}
@@ -177,62 +177,67 @@ export default function WorkingOld() {
                 {/* <View style={[styles.surveyTablePopUp, { top: 280 , left: 270}]} /> */}
 
                 {pins?.length > 0
-                    ? pins.map((i,index) => (
-                          <TouchableOpacity
+                    ? pins.map((i, index) => (
+                        <TouchableOpacity
                             key={index}
-                              onPress={() => onPressSavedPin(i)}
-                              style={[styles.pinWrapper, styles.surveyTablePopUp, { top: i.y, left: i.x }]}
-                              onLongPress={() => onPressSavedPin(i)}
-                              onPressOut={() => onPressSavedPin(i)}
-                          >
-                              <Pin shadow = {true} />
-                          </TouchableOpacity>
-                      ))
+                            onPress={() => onPressSavedPin(i)}
+                            style={[styles.pinWrapper, styles.surveyTablePopUp, { top: i.y, left: i.x }]}
+                            onLongPress={() => onPressSavedPin(i)}
+                            onPressOut={() => onPressSavedPin(i)}
+                        >
+                            <Pin shadow={true} />
+                        </TouchableOpacity>
+                    ))
                     : null}
 
                 {activeEditDraggable && (
                     <Draggable x={currentDraggerPosition.x} y={currentDraggerPosition.y} onDragRelease={() => releaseDrag('edit')}>
                         <View style={[styles.pinWrapper]} ref={editDragRef} >
-                            <Pin dragging={activeNewDraggableId > 0 } shadow = {false} />
+                            <Pin dragging={activeNewDraggableId > 0} shadow={false} />
                         </View>
                     </Draggable>
                 )}
             </View>
-            <View style={[ activeNewDraggable && styles.initialWrapper]} >
+            <View style={[activeNewDraggable && styles.initialWrapper]} >
                 {activeNewDraggable && (
                     <Draggable x={20} y={0} onDragRelease={releaseDrag} onDrag={() => dragStart(-1)}>
                         <View style={[styles.pinWrapper]} ref={newDragRef}>
-                            <Pin dragging={activeNewDraggableId === -1} shadow = {false} />
+                            <Pin dragging={activeNewDraggableId === -1} shadow={false} />
                         </View>
                     </Draggable>
                 )}
             </View>
             <View>
                 {dropPin && (
-                    <View>
-                        <TouchableOpacity onPress={savePin} style={styles.buttonWrapper}>
+                    <View style={{ position: "relative", width: '100%', alignItems: 'center', height: 200, }}>
+                        <TouchableOpacity onPress={savePin} >
                             <Text>SAVE</Text>
                         </TouchableOpacity>
-                        <TouchableOpacity onPress={deleteItem} style={styles.buttonWrapper}>
+                        <TouchableOpacity onPress={deleteItem} >
                             <Text>DELETE</Text>
                         </TouchableOpacity>
-                        <TouchableOpacity onPress={up} style={styles.buttonWrapper}>
-                            <Text>UP</Text>
+
+                        <TouchableOpacity onPress={up} >
+                            <Image source={require("./btn.png")} style={[styles.btnImg, styles.topArrow]} />
                         </TouchableOpacity>
-                        <TouchableOpacity onPress={down} style={styles.buttonWrapper}>
-                            <Text>DOWN</Text>
+
+                        <TouchableOpacity onPress={down} >
+                            <Image source={require("./btn.png")} style={[styles.btnImg, styles.downArrow]} />
                         </TouchableOpacity>
-                        <TouchableOpacity onPress={left} style={styles.buttonWrapper}>
-                            <Text>LEFT</Text>
+                        <TouchableOpacity onPress={left} >
+                            <Image source={require("./btn.png")} style={[styles.btnImg, styles.leftArrow]} />
                         </TouchableOpacity>
-                        <TouchableOpacity onPress={right} style={styles.buttonWrapper}>
-                            <Text>RIGHT</Text>
+                        <TouchableOpacity onPress={right} >
+                            <Image source={require("./btn.png")} style={[styles.btnImg, styles.rightArrow]} />
                         </TouchableOpacity>
-                        
+
+
                     </View>
+
+
                 )}
             </View>
-            </View>
+        </View>
     );
 }
 
@@ -243,13 +248,13 @@ const styles = StyleSheet.create({
         backgroundColor: "#fff",
         alignItems: "center",
         justifyContent: "center",
-        // height: 850,
+
     },
     imageWrapper: {
         borderColor: "#0a0a0a",
         borderWidth: 2,
-        width: 604,
-        height: 604,
+        width: 404,
+        height: 404,
     },
     buttonWrapper: {
         borderColor: "#0a0a0a",
@@ -290,21 +295,18 @@ const styles = StyleSheet.create({
         elevation: 10,
     },
     imageStyles: {
-        width: 600,
-        height: 600,
+        width: 400,
+        height: 400,
     },
     surveyTablePopUp: {
         alignItems: "center",
         position: "absolute",
+
     },
     alignItemCenter: {
         alignItems: "center",
     },
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
+
     draggable: {
         width: 100,
         height: 100,
@@ -315,7 +317,7 @@ const styles = StyleSheet.create({
         height: 100,
         backgroundColor: "green",
     },
-    initialWrapper : {
+    initialWrapper: {
         height: 60,
         width: 60,
         borderColor: "#0a0a0a",
@@ -323,5 +325,40 @@ const styles = StyleSheet.create({
         borderRadius: 100,
         top: 80,
         left: 30
-    }
+    },
+
+    btnImg: {
+        width: 27,
+        height: 55,
+        position: 'absolute',
+    },
+
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1
+    },
+    rightArrow: {
+        transform: [{ rotate: '180deg' }],
+        left: 127,
+        top: 70,
+
+    },
+    leftArrow: {
+        transform: [{ rotate: '360deg' }],
+        left: 45,
+        top: 70,
+    },
+
+    topArrow: {
+        transform: [{ rotate: '90deg' }],
+        left: 85,
+        top: 30,
+    },
+
+    downArrow: {
+        transform: [{ rotate: '270deg' }],
+        top: 110,
+        left: 85,
+    },
 });
